7ab9df2833151a421e4bd18b18f3d4d7028ad922
jevtovicc <jevtovic99@yahoo.com>
2022-04-22 15:48:32 +0200

    Implement support for castling based on castling rules

--- src/clojure_chess_engine/core.clj
+++ src/clojure_chess_engine/core.clj
@@ -29,11 +29,19 @@
       "w" :white
       "b" :black)))
 
+(defn parse-castling [fen]
+  (last (str/split fen #" ")))
+
 (defn fen->game-state [fen]
   (let [board (parse->board fen)
-        player (parse-player fen)]
+        player (parse-player fen)
+        castling-info (parse-castling fen)]
     {:board board
-     :player-on-move player}))
+     :player-on-move player
+     :white-can-castle-ks? (str/includes? castling-info "K")
+     :white-can-castle-qs? (str/includes? castling-info "Q")
+     :black-can-castle-ks? (str/includes? castling-info "k")
+     :black-can-castle-qs? (str/includes? castling-info "q")}))
 
 (def starting-fen "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq")
 (def starting-game-state (fen->game-state starting-fen))
@@ -138,23 +146,21 @@
 (defn id->square [s]
   (map #(Integer/parseInt %) (str/split s #"-")))
 
-(defmulti get-pseudolegal-destinations (fn [board from-sq] (get-piece board from-sq)))
+(defmulti get-pseudolegal-destinations (fn [game-state from-sq] (get-piece (:board game-state) from-sq)))
 
 (defmethod get-pseudolegal-destinations :n
-  [board from-sq]
+  [{board :board} from-sq]
   (->> knight-directions
        (map #(add-squares from-sq %))
        (filter square-on-board?)
-       (remove #(same-piece-color? :n (get-piece board %)))
-       #_set))
+       (remove #(same-piece-color? :n (get-piece board %)))))
 
 (defmethod get-pseudolegal-destinations :N
-  [board from-sq]
+  [{board :board} from-sq]
   (->> knight-directions
        (map #(add-squares from-sq %))
        (filter square-on-board?)
-       (remove #(same-piece-color? :N (get-piece board %)))
-       #_set))
+       (remove #(same-piece-color? :N (get-piece board %)))))
 
 (defn take-while+
   [pred coll]
@@ -176,31 +182,31 @@
   (mapcat #(get-squares-in-direction board from-sq %) dirs))
 
 (defmethod get-pseudolegal-destinations :r
-  [board from-sq]
+  [{board :board} from-sq]
   (get-squares-in-directions board from-sq rook-directions))
 
 (defmethod get-pseudolegal-destinations :R
-  [board from-sq]
+  [{board :board} from-sq]
   (get-squares-in-directions board from-sq rook-directions))
 
 (defmethod get-pseudolegal-destinations :b
-  [board from-sq]
+  [{board :board} from-sq]
   (get-squares-in-directions board from-sq bishop-directions))
 
 (defmethod get-pseudolegal-destinations :B
-  [board from-sq]
+  [{board :board} from-sq]
   (get-squares-in-directions board from-sq bishop-directions))
 
 (defmethod get-pseudolegal-destinations :q
-  [board from-sq]
+  [{board :board} from-sq]
   (get-squares-in-directions board from-sq all-directions))
 
 (defmethod get-pseudolegal-destinations :Q
-  [board from-sq]
+  [{board :board} from-sq]
   (get-squares-in-directions board from-sq all-directions))
 
 (defmethod get-pseudolegal-destinations :p
-  [board from-sq]
+  [{board :board} from-sq]
   (let [not-moved? (= (first from-sq) 1)
         one-up (add-squares dir-up from-sq)
         two-up (add-squares dir-up one-up)
@@ -219,7 +225,7 @@
                    set))))
 
 (defmethod get-pseudolegal-destinations :P
-  [board from-sq]
+  [{board :board} from-sq]
   (let [not-moved? (= (first from-sq) 6)
         one-down (add-squares dir-down from-sq)
         two-down (add-squares dir-down one-down)
@@ -236,48 +242,86 @@
            (not (same-piece-color? :P (get-piece board one-down-right)))) (conj one-down-right)
       :always (->> (filter square-on-board?)))))
 
+(declare squares-attacked-by-player)
+(declare in-check?)
+
+(defn castling-squares [side]
+  (condp = side
+    :k #{[0 5] [0 6]}
+    :q #{[0 1] [0 2] [0 3]}
+    :K #{[7 5] [7 6]}
+    :Q #{[7 1] [7 2] [7 3]}))
+
+(defn castling-squares-empty? [board side]
+  (->> side
+       castling-squares
+       (every? #(square-empty? board %))))
+
+(defn my-any?
+  "Returns true if some element in coll satisfies predicate"
+  [pred col]
+  (not (not-any? pred col)))
+
+(defn castling-squares-attacked? [{player-on-move :player-on-move :as game-state} side]
+  (let [attacked-squares (squares-attacked-by-player game-state (get-opponent player-on-move))
+        castling-squares (castling-squares side)]
+    (my-any? castling-squares attacked-squares)))
+
+(defn castling-possible? [game-state side]
+  (and (castling-squares-empty? (:board game-state) side)
+       (not (castling-squares-attacked? game-state side))
+       (not (in-check? game-state))))
+
 (defmethod get-pseudolegal-destinations :k
-  [board from-sq]
-  (->> all-directions
-       (map #(add-squares from-sq %))
-       (filter square-on-board?)
-       (remove #(same-piece-color? :k (get-piece board %)))))
+  [{:keys [board black-can-castle-ks? black-can-castle-qs?] :as game-state} from-sq]
+  (cond-> []
+    (and black-can-castle-ks? (castling-possible? game-state :k)) (conj [0 6])
+    (and black-can-castle-qs? (castling-possible? game-state :q)) (conj [0 1])
+    :always (into
+             (->> all-directions
+                  (map #(add-squares from-sq %))
+                  (filter square-on-board?)
+                  (remove #(same-piece-color? :k (get-piece board %)))))))
 
 (defmethod get-pseudolegal-destinations :K
-  [board from-sq]
-  (->> all-directions
-       (map #(add-squares from-sq %))
-       (filter square-on-board?)
-       (remove #(same-piece-color? :K (get-piece board %)))))
+  [{:keys [board white-can-castle-ks? white-can-castle-qs?] :as game-state} from-sq]
+  (cond-> []
+    (and white-can-castle-ks? (castling-possible? game-state :K)) (conj [7 6])
+    (and white-can-castle-qs? (castling-possible? game-state :Q)) (conj [7 1])
+    :always (into
+             (->> all-directions
+                  (map #(add-squares from-sq %))
+                  (filter square-on-board?)
+                  (remove #(same-piece-color? :K (get-piece board %)))))))
 
-(defn squares-attacked-by-player [board player]
+(defn squares-attacked-by-player [{board :board :as game-state} player]
   (->> board
        occupied-squares
        (filter #(= player (piece-color (get-piece board %))))
        (remove #(contains? #{:k :K} (get-piece board %)))
-       (mapcat #(get-pseudolegal-destinations board %))
+       (mapcat #(get-pseudolegal-destinations game-state %))
        distinct))
 
-(defn in-check? [board player]
-  (let [attacked-king (get-players-king player)
-        opponent (get-opponent player)]
-    (as-> (squares-attacked-by-player board opponent) xs
+(defn in-check? [{:keys [board player-on-move] :as game-state}]
+  (let [attacked-king (get-players-king player-on-move)
+        opponent (get-opponent player-on-move)]
+    (as-> (squares-attacked-by-player game-state opponent) xs
       (map #(get-piece board %) xs)
       (set xs)
       (contains? xs attacked-king))))
 
-(defn in-check-after-move? [board player from-sq to-sq]
-  (in-check? (move-piece board from-sq to-sq) player))
+(defn in-check-after-move? [{board :board :as game-state} from-sq to-sq]
+  (in-check? (assoc game-state :board (move-piece board from-sq to-sq))))
 
-(defn get-legal-destinations [board player from-sq]
-  (->> (get-pseudolegal-destinations board from-sq)
-       (remove #(in-check-after-move? board player from-sq %))))
+(defn get-legal-destinations [game-state from-sq]
+  (->> (get-pseudolegal-destinations game-state from-sq)
+       (remove #(in-check-after-move? game-state from-sq %))))
 
-(defn check-mate? [board player]
+(defn check-mate? [{:keys [board player-on-move] :as game-state}]
   (->> board
        occupied-squares
-       (filter #(= (piece-color (get-piece board %)) player))
-       (mapcat #(get-legal-destinations board player %))
+       (filter #(= (piece-color (get-piece board %)) player-on-move))
+       (mapcat #(get-legal-destinations game-state %))
        distinct
        empty?))
 
@@ -321,11 +365,6 @@
   (let [selected-square (first (seesaw/select board-pane [:.square-selected]))]
     (seesaw/config! selected-square :class :square-illegal :border nil)))
 
-(defn switch-player [player]
-  (condp = player
-    :white :black
-    :black :white))
-
 (defn reset-game-state []
   (reset! game-state starting-game-state))
 
@@ -333,17 +372,15 @@
   (reset-game-state)
   (seesaw/config! board-pane :items (generate-starting-buttons)))
 
-
-(defn possible-moves [board player]
+(defn possible-moves [{:keys [board player-on-move] :as game-state}]
   (->> all-squares
-       (filter #(= (piece-color (get-piece board %)) player))
+       (filter #(= (piece-color (get-piece board %)) player-on-move))
        (reduce
         (fn [acc square]
-          (assoc acc square (get-legal-destinations board player square)))
+          (assoc acc square (get-legal-destinations game-state square)))
         {})
        (remove (fn [move] (empty? (val move))))
        (into {})))
-
 
 (defn disable-buttons []
   (dorun (map #(seesaw/config! % :enabled? false) (seesaw/select board-pane [:JButton]))))
@@ -358,15 +395,14 @@
        keyword))
 
 (defn generate-random-move []
-  (let [moves (possible-moves (:board @game-state) :black)
+  (let [moves (possible-moves @game-state)
         from-sq (rand-nth (keys moves))
         to-sq (rand-nth (moves from-sq))
         src-button (seesaw/select board-pane [(square->seesawid from-sq)])
         dest-button (seesaw/select board-pane [(square->seesawid to-sq)])]
     (seesaw/config! dest-button :icon (seesaw/config src-button :icon))
     (seesaw/config! src-button :icon nil)
-    (swap! game-state #(assoc % :board (move-piece (:board %) from-sq to-sq) :player-on-move (switch-player (:player-on-move %))))))
-
+    (swap! game-state #(assoc % :board (move-piece (:board %) from-sq to-sq) :player-on-move (get-opponent (:player-on-move %))))))
 
 ;; Slucajevi
 ;; 1) Ako nije selektovana figura - selektuj figuru (stavi joj klasu :square-selected)
@@ -377,20 +413,62 @@
   (let [id (seesaw/config e :id)
         square (id->square (name id))]
     (cond
-      (contains? (seesaw/config e :class) "square-legal") (let [selected-square (first (seesaw/select board-pane [:.square-selected]))]
+      (contains? (seesaw/config e :class) "square-legal") (let [selected-square (first (seesaw/select board-pane [:.square-selected]))
+                                                                from-sq (id->square (name (seesaw/config selected-square :id)))
+                                                                piece (get-piece (:board @game-state) from-sq)]
                                                             (seesaw/config! e :icon (seesaw/config selected-square :icon))
                                                             (seesaw/config! selected-square :icon nil)
                                                             (untag-legal-squares)
                                                             (untag-selected-square)
-                                                            (swap! game-state #(assoc % :board (move-piece (:board %) (id->square (name (seesaw/config selected-square :id))) square) :player-on-move (switch-player (:player-on-move %))))
-                                                            (if (check-mate? (:board @game-state) (:player-on-move @game-state))
+                                                            (swap! game-state #(assoc % :board (move-piece (:board %) from-sq square) :player-on-move (get-opponent (:player-on-move %))))
+                                                            (if (and (= piece :k)
+                                                                     (= from-sq [0 4])
+                                                                     (= square [0 1]))
+                                                              (do
+                                                                (swap! game-state #(assoc % :board (move-piece (:board %) [0 0] [0 2])))
+                                                                (seesaw/config! (seesaw/select board-pane [:#0-2]) :icon (seesaw/config (seesaw/select board-pane [:#0-0]) :icon))
+                                                                (seesaw/config! (seesaw/select board-pane [:#0-0]) :icon nil)))
+                                                            (if (and (= piece :k)
+                                                                     (= from-sq [0 4])
+                                                                     (= square [0 6]))
+                                                              (do
+                                                                (swap! game-state #(assoc % :board (move-piece (:board %) [0 7] [0 5])))
+                                                                (seesaw/config! (seesaw/select board-pane [:#0-5]) :icon (seesaw/config (seesaw/select board-pane [:#0-7]) :icon))
+                                                                (seesaw/config! (seesaw/select board-pane [:#0-7]) :icon nil)))
+                                                            (if (= piece :k)
+                                                              (swap! game-state #(assoc % :black-can-castle-ks? false :black-can-castle-qs? false)))
+                                                            (if (and (= piece :K)
+                                                                     (= from-sq [7 4])
+                                                                     (= square [7 1]))
+                                                              (do
+                                                                (swap! game-state #(assoc % :board (move-piece (:board %) [7 0] [7 2])))
+                                                                (seesaw/config! (seesaw/select board-pane [:#7-2]) :icon (seesaw/config (seesaw/select board-pane [:#7-0]) :icon))
+                                                                (seesaw/config! (seesaw/select board-pane [:#7-0]) :icon nil)))
+                                                            (if (and (= piece :K)
+                                                                     (= from-sq [7 4])
+                                                                     (= square [7 6]))
+                                                              (do
+                                                                (swap! game-state #(assoc % :board (move-piece (:board %) [7 7] [7 5])))
+                                                                (seesaw/config! (seesaw/select board-pane [:#7-5]) :icon (seesaw/config (seesaw/select board-pane [:#7-7]) :icon))
+                                                                (seesaw/config! (seesaw/select board-pane [:#7-7]) :icon nil)))
+                                                            (if (= piece :K)
+                                                              (swap! game-state #(assoc % :white-can-castle-ks? false :white-can-castle-qs? false)))
+                                                            (if (= from-sq [0 0])
+                                                              (swap! game-state #(assoc % :black-can-castle-qs? false)))
+                                                            (if (= from-sq [0 7])
+                                                              (swap! game-state #(assoc % :black-can-castle-ks? false)))
+                                                            (if (= from-sq [7 0])
+                                                              (swap! game-state #(assoc % :white-can-castle-qs? false)))
+                                                            (if (= from-sq [7 7])
+                                                              (swap! game-state #(assoc % :white-can-castle-ks? false)))
+                                                            (if (check-mate? @game-state)
                                                               (do
                                                                 (seesaw/alert "Game over!")
                                                                 (reset-game))
                                                               (generate-random-move)))
       (square-empty? (:board @game-state) square) (do (untag-legal-squares)
                                                       (untag-selected-square))
-      :else (let [legal-moves (get-legal-destinations (:board @game-state) (:player-on-move @game-state) square)]
+      :else (let [legal-moves (get-legal-destinations @game-state square)]
               (untag-legal-squares)
               (untag-selected-square)
               (seesaw/config! e :class :square-selected :border (ssborder/line-border :thickness 4 :color :green))
